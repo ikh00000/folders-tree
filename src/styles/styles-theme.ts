9dec73e94c8a3fe2cfa8532a38f559fa11914954
ikh00000 <vanja.khizhnjak99@gmail.com>
2024-07-26 21:51:08 +0200

    Add global style variables

---
+++ src/styles/styles-theme.ts
@@ -0,0 +1,28 @@
+const theme = {
+  colors: {
+    primary: '#007bff',
+    primaryHover: '#499af2',
+    secondary: '#fff',
+    warning: '#ffcf40',
+    primaryText: '#333',
+    secondaryText: '#fff',
+    blueText: '#007bff',
+    grayBackground: '#f6f6f6',
+    placeholder: '#757575',
+  },
+  borderRadius: '0.5rem',
+  border: '1px solid #ccc',
+  iconWidth: '15px',
+  fontSize: {
+    small: '0.8rem',
+    medium: '1rem',
+    large: '1.2rem',
+    xlarge: '1.5rem',
+  },
+  fonts: {
+    main: "'Roboto', sans-serif",
+    code: 'monospace',
+  },
+};
+
+export default theme;
