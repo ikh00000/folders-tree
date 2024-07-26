3fe1b51afd1329b4533571dfe4ad17b52442897e
ikh00000 <vanja.khizhnjak99@gmail.com>
2024-07-26 21:45:24 +0200

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  const isProduction = process.env.NODE_ENV === 'production';
+  const isDevelopment = process.env.NODE_ENV === 'development';
+  const port = isProduction ? 80 : 5173;
+
+  return {
+    base: isProduction ? '/folders-tree/' : '/',
+    plugins: [react()],
+    server: {
+      port,
+      host: true,
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: isDevelopment,
+    },
+  };
+});
